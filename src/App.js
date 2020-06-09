5ad747507d71ff7f2a07e280de248a8365cb9770
soadtech <fernandojoseropero@gmail.com>
2020-06-09 16:01:40 -0500

    version desktop terminada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import QuoteContainer from "./components/QuoteContainer";
+import ImageContainer from "./components/ImageContainer";
+import { users } from "./config/users";
+
+import "./css/main.css";
+import "./css/reset.css";
 
 function App() {
+  const [userSelect, setUserSelect] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <QuoteContainer
+        quote={users[userSelect].quote}
+        name={users[userSelect].name}
+        profession={users[userSelect].profession}
+      />
+      <ImageContainer
+        image={users[userSelect].image}
+        userSelect={userSelect}
+        setUserSelect={setUserSelect}
+        users={users}
+      />
     </div>
   );
 }
